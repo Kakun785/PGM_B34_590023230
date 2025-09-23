39439adfbadd1bb9e5b9f2d8ccd594c1f8868c5e
Kakun785 <Kakunchauhan433@gmail.com>
2025-09-23 18:45:06 +0530

    Add files via upload
    Add ques 3 and 4

---
+++ main (2).c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+int main(){
+
+
+
+
+    float length ,width ,area, perimeter;
+    printf("enter the value of  l : ");
+    scanf("%f", &length);
+    printf("enter the value of  b :");
+    scanf("%f", &width);
+    
+    area = length * width;
+    perimeter= 2*(length + width);
+    printf("Area of a rectangle is %.2f\n", area);
+    scanf("Area of a perimeter is %.2f\n", perimeter);
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
---
+++ main (3).c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+int main(){
+
+
+
+
+    float radius, area, circ;
+    printf("enter the value of radius : ");
+    scanf("%f", &radius);
+    
+    area = 22/7 * radius * radius;
+    
+    circ = 2 * 22/7 * radius; 
+    
+    
+    printf("\n Area of a circle is %.2f", area);
+    printf("\n Circumfrence of a circle %.2f", circ);
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
