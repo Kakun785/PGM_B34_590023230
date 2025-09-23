d20ba56c07c7602d54e01a92a853b358f863b2ec
Kakun785 <Kakunchauhan433@gmail.com>
2025-09-23 18:16:17 +0530

    Add files via upload
    Add Question 1 and Question 2

---
+++ main (1).c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+int main(){
+
+
+
+
+    int a,b,sum,diff,product,quo;
+    printf("enter the value of a : ");
+    scanf("%d", &a);
+    printf("enter the value of b : ");
+    scanf("%d", &b);
+    
+    printf("sum %d\n", a+b);
+    printf(" diff %d\n", a-b);
+    printf("product %d\n", a/b);
+    printf("quo %d\n", a/b);
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
