19aebe0b38f0680a29b09e35ca7300504d961e39
Kakun785 <Kakunchauhan433@gmail.com>
2025-09-23 19:35:15 +0530

    Add files via upload
    Add Question 13 and 14

---
+++ main (13).c
@@ -0,0 +1,74 @@
+
+#include <stdio.h>
+int main (){
+    
+   
+
+    char ch;
+
+    // Ask the user to enter a character
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    // Check each vowel separately using if-else if-else
+    if (ch == 'a' || ch == 'A') {
+        printf("%c is a vowel.\n", ch);
+    } else if (ch == 'e' || ch == 'E') {
+        printf("%c is a vowel.\n", ch);
+    } else if (ch == 'i' || ch == 'I') {
+        printf("%c is a vowel.\n", ch);
+    } else if (ch == 'o' || ch == 'O') {
+        printf("%c is a vowel.\n", ch);
+    } else if (ch == 'u' || ch == 'U') {
+        printf("%c is a vowel.\n", ch);
+    } else {
+        printf("%c is not a vowel.\n", ch);
+    }
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
