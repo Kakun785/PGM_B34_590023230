4fcbecc8c998cc3574ca853801c058db3c92d9b3
Kakun785 <Kakunchauhan433@gmail.com>
2025-10-18 19:05:28 +0530

    Day 5) Ques 10)

--- main (9).c
+++ main (9).c
@@ -4,16 +4,16 @@ int main (){
 int totalSeconds;
     int hours, minutes, seconds;
 
-    // Input
+    
     printf("Enter time in seconds: ");
     scanf("%d", &totalSeconds);
 
-    // Conversion
+   
     hours = totalSeconds / 3600;
     minutes = (totalSeconds % 3600) / 60;
     seconds = totalSeconds % 60;
 
-    // Output
+    
     printf("Time = %02d:%02d:%02d \n", hours, minutes, seconds);
     return 0;
 }
